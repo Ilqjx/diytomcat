476a240dfb0519949932c816436e88b5d4e02a36
Ilqjx <1695151719@qq.com>
2020-09-06 21:08:01 +0800

    Hello DIYTomcat

---
+++ src/cn/ilqjx/diytomcat/Bootstrap.java
@@ -0,0 +1,48 @@
+package cn.ilqjx.diytomcat;
+
+import cn.hutool.core.util.NetUtil;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @author upfly
+ * @create 2020-09-06 20:42
+ */
+public class Bootstrap {
+
+    public static void main(String[] args) {
+        try {
+            int port = 18080;
+
+            if (!NetUtil.isUsableLocalPort(port)) { // 判断端口是否被占用，没被占用返回true
+                System.out.println(port + "端口已经被占用了，排查并关闭本端口的办法请用：\r\nhttps://how2j.cn/k/tomcat/tomcat-portfix/545.html");
+                return;
+            }
+            ServerSocket ss = new ServerSocket(port);
+
+            while (true) {
+                Socket s =  ss.accept();
+                InputStream is = s.getInputStream();
+                int bufferSize = 1024;
+                byte[] buffer = new byte[bufferSize];
+                is.read(buffer);
+                String requestString = new String(buffer,"utf-8");
+                System.out.println("浏览器的输入信息： \r\n" + requestString);
+
+                OutputStream os = s.getOutputStream();
+                String response_head = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/html\r\n\r\n";
+                String responseString = "Hello DIY Tomcat from how2j.cn";
+                responseString = response_head + responseString;
+                os.write(responseString.getBytes());
+                os.flush();
+                s.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
