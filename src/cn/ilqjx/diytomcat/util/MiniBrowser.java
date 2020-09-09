5b64ef026312e52a0119d0c162a6e30698558e6b
Ilqjx <1695151719@qq.com>
2020-09-09 20:57:16 +0800

    MiniBrowser

--- src/cn/ilqjx/diytomcat/util/MiniBrowser.java
+++ src/cn/ilqjx/diytomcat/util/MiniBrowser.java
@@ -1,10 +1,12 @@
 package cn.ilqjx.diytomcat.util;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.InetSocketAddress;
-import java.net.MalformedURLException;
 import java.net.Socket;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author upfly
@@ -25,6 +27,13 @@ public class MiniBrowser {
         byte[] response = getHttpBytes(url, gzip);
     }
 
+    /**
+     * 返回二进制的http响应
+     *
+     * @param url
+     * @param gzip 可以获取压缩后的数据
+     * @return
+     */
     public static byte[] getHttpBytes(String url, boolean gzip) {
         byte[] result = null;
         try {
@@ -39,10 +48,62 @@ public class MiniBrowser {
             // 将此套接字连接到服务器
             client.connect(inetSocketAddress, 1000);
 
-        } catch (MalformedURLException e) {
+            Map<String, String> requestHeaders = new HashMap<>();
+            requestHeaders.put("Host", u.getHost() + ":" + port);
+            requestHeaders.put("Accept", "text/html");
+            requestHeaders.put("Connection", "close");
+            requestHeaders.put("User-Agent", "how2j mini browser / java1.8");
+
+            if (gzip) {
+                requestHeaders.put("Accept-Encoding", "gzip");
+            }
+
+            // 获取此url的路径部分
+            String path = u.getPath();
+            if (path.length() == 0) {
+                path = "/";
+            }
+
+            String firstLine = "GET" + path + "HTTP/1.1\r\n";
+
+            StringBuffer httpRequestString = new StringBuffer();
+            httpRequestString.append(firstLine);
+            Set<String> headers = requestHeaders.keySet();
+            for (String header : headers) {
+                String headerLine = header + ":" + requestHeaders.get(header) + "\r\n";
+                httpRequestString.append(headerLine);
+            }
+
+            // autoFlush: true --> 刷新输出缓存区
+            PrintWriter printWriter = new PrintWriter(client.getOutputStream(), true);
+            // 输出到服务器？？？
+            printWriter.println(httpRequestString);
+            InputStream is = client.getInputStream();
+
+            int buffer_size = 1024;
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            byte[] buffer = new byte[buffer_size];
+            while (true) {
+                int length = is.read(buffer);
+                if (length == -1) {
+                    break;
+                }
+                baos.write(buffer, 0, length);
+                if (length != buffer_size) {
+                    break;
+                }
+            }
+
+            result = baos.toByteArray();
+            client.close();
+        } catch (Exception e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            try {
+                result = e.toString().getBytes("utf-8");
+            } catch (UnsupportedEncodingException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 }
