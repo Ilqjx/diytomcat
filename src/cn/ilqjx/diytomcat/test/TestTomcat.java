a768c5a4077b9f0ba2d579126d1df2396c029b8b
Ilqjx <1695151719@qq.com>
2020-09-11 20:47:38 +0800

    Request对象

---
+++ src/cn/ilqjx/diytomcat/test/TestTomcat.java
@@ -0,0 +1,44 @@
+package cn.ilqjx.diytomcat.test;
+
+import cn.hutool.core.util.NetUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.ilqjx.diytomcat.util.MiniBrowser;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author upfly
+ * @create 2020-09-11 19:30
+ */
+public class TestTomcat {
+    private static int port = 18080;
+    private static String ip = "127.0.0.1";
+
+    @BeforeClass
+    public static void beforeClass() {
+        // 测试开始前检查 diytomcat 是否已经启动
+        if (NetUtil.isUsableLocalPort(port)) { // 检查端口是否可用
+            // 错误输出
+            System.err.println("请先启动位于端口：" + port + " 的diytomcat，否则不会进行单元测试.");
+            // 标准输出
+            // System.out.println("请先启动位于端口：" + port + " 的diytomcat，否则不会进行单元测试.");
+            // 非0表示非正常退出程序，0表示正常退出程序
+            System.exit(1);
+        } else {
+            System.out.println("检测到 diytomcat 已经启动，下面进行单元测试.");
+        }
+    }
+
+    @Test
+    public void testHelloTomcat() {
+        String content = getContentString("/");
+        Assert.assertEquals(content, "Hello DIY Tomcat from how2j.cn");
+    }
+
+    private String getContentString(String uri) {
+        String url = StrUtil.format("http://{}:{}{}", ip, port, uri);
+        String content = MiniBrowser.getContentString(url);
+        return content;
+    }
+}
