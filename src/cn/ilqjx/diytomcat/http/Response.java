d709c598c8b99ef717652c02665ce3e3a28161d7
Ilqjx <1695151719@qq.com>
2020-10-08 18:45:41 +0800

    BaseRequest & BaseResponse

--- src/cn/ilqjx/diytomcat/http/Response.java
+++ src/cn/ilqjx/diytomcat/http/Response.java
@@ -1,20 +1,14 @@
 package cn.ilqjx.diytomcat.http;
 
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
-import java.util.Collection;
-import java.util.Locale;
 
 /**
  * @author upfly
  * @create 2020-09-12 20:44
  */
-public class Response implements HttpServletResponse {
+public class Response extends BaseResponse {
     // StringWriter: 把数据写入到 String 中去，内部提供 StringBuffer 保存数据
     private StringWriter stringWriter; // 用于存放返回的 html 文本
     private PrintWriter writer; // 打印流
@@ -33,72 +27,12 @@ public class Response implements HttpServletResponse {
         return writer;
     }
 
-    @Override
-    public void setCharacterEncoding(String s) {
-
-    }
-
-    @Override
-    public void setContentLength(int i) {
-
-    }
-
     public void setContentType(String contentType) {
         this.contentType = contentType;
     }
 
-    @Override
-    public void setBufferSize(int i) {
-
-    }
-
-    @Override
-    public int getBufferSize() {
-        return 0;
-    }
-
-    @Override
-    public void flushBuffer() throws IOException {
-
-    }
-
-    @Override
-    public void resetBuffer() {
-
-    }
-
-    @Override
-    public boolean isCommitted() {
-        return false;
-    }
-
-    @Override
-    public void reset() {
-
-    }
-
-    @Override
-    public void setLocale(Locale locale) {
-
-    }
-
-    @Override
-    public Locale getLocale() {
-        return null;
-    }
-
-    @Override
-    public String getCharacterEncoding() {
-        return null;
-    }
-
     public String getContentType() {
         return contentType;
-    }
-
-    @Override
-    public ServletOutputStream getOutputStream() throws IOException {
-        return null;
     }
 
     public void setBody(byte[] body) {
@@ -111,110 +45,5 @@ public class Response implements HttpServletResponse {
             body = content.getBytes("utf-8");
         }
         return body;
-    }
-
-    @Override
-    public void addCookie(Cookie cookie) {
-
-    }
-
-    @Override
-    public boolean containsHeader(String s) {
-        return false;
-    }
-
-    @Override
-    public String encodeURL(String s) {
-        return null;
-    }
-
-    @Override
-    public String encodeRedirectURL(String s) {
-        return null;
-    }
-
-    @Override
-    public String encodeUrl(String s) {
-        return null;
-    }
-
-    @Override
-    public String encodeRedirectUrl(String s) {
-        return null;
-    }
-
-    @Override
-    public void sendError(int i, String s) throws IOException {
-
-    }
-
-    @Override
-    public void sendError(int i) throws IOException {
-
-    }
-
-    @Override
-    public void sendRedirect(String s) throws IOException {
-
-    }
-
-    @Override
-    public void setDateHeader(String s, long l) {
-
-    }
-
-    @Override
-    public void addDateHeader(String s, long l) {
-
-    }
-
-    @Override
-    public void setHeader(String s, String s1) {
-
-    }
-
-    @Override
-    public void addHeader(String s, String s1) {
-
-    }
-
-    @Override
-    public void setIntHeader(String s, int i) {
-
-    }
-
-    @Override
-    public void addIntHeader(String s, int i) {
-
-    }
-
-    @Override
-    public void setStatus(int i) {
-
-    }
-
-    @Override
-    public void setStatus(int i, String s) {
-
-    }
-
-    @Override
-    public int getStatus() {
-        return 0;
-    }
-
-    @Override
-    public String getHeader(String s) {
-        return null;
-    }
-
-    @Override
-    public Collection<String> getHeaders(String s) {
-        return null;
-    }
-
-    @Override
-    public Collection<String> getHeaderNames() {
-        return null;
     }
 }
